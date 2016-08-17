08a9b9809835d726d3abaf0967f1a04b4206a35a
Bill Anton <anton@primedataintelligence.com>
2016-08-16 18:53:49 -0700

    added table and stored procedure to load xEvent Class/Subclass descriptions into staging table

---
+++ SsasEventsAnalyzerDB/Schemas/stg/Tables/xEventDecode.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [stg].[xEventDecode] (
+     EventClassId				INT NOT NULL,
+     EventSubclassId			INT NOT NULL,
+     EventClassName				VARCHAR(50) NULL,
+     EventSubclassName			VARCHAR(50) NULL,
+     EventClassDescription		VARCHAR(500) NULL,
+     CONSTRAINT PK_stg_xEventDecode PRIMARY KEY CLUSTERED ( EventClassId, EventSubclassId )
+);
